df7e865aee8e587a914c47f374206da8513471fc
Encheww77 <anglichaneca@gmail.com>
2024-11-06 20:04:59 +0200

    Add files via upload

---
+++ storages.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2024 at 08:05 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `storageunits`
+--
+
+CREATE TABLE `storages` (
+  `id` int(50) DEFAULT NULL,
+  `owner` varchar(46) DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `expiring_date` varchar(50) NOT NULL,
+  `tier` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
